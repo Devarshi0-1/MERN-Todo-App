357a817c301f1bcda3d00f860868d7d9339abb3e
Devarshi <devarshidwi@gmail.com>
2023-08-29 01:39:01 +0530

    Testing Optimistic Updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 	const { isAuthenticated } = useContext(Context);
 
 	const handleUpdate = async (id) => {
-		console.log(tasks);
+		console.log(...tasks[0]);
 		// setTasks((prev) => {
 		// 	return prev.map((task) =>
 		// 		task._id === id ? { ...task, isCompleted: !isCompleted } : task
